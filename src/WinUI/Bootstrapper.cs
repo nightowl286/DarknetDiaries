804da20f776c44fb559d2c253f05865ddfaa8282
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2022-01-11 06:32:14 +0000

    Configured the UI project to use Caliburn.Micro.

---
+++ src/WinUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using DarknetDiaries.WinUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DarknetDiaries.WinUI
+{
+   internal class Bootstrapper : BootstrapperBase
+   {
+      #region Bootstrapper basics
+      public Bootstrapper()
+      {
+         Initialize();
+      }
+      protected override void OnStartup(object sender, StartupEventArgs e)
+      {
+         DisplayRootViewFor<ShellViewModel>();
+      }
+      #endregion
+   }
+}
