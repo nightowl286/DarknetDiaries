ed52a2cf50cbca66730e42bd220de5ac3aa9352f
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2022-01-11 09:40:32 +0000

    Created a refresh feed functionality in the shell view model.

--- src/WinUI/Bootstrapper.cs
+++ src/WinUI/Bootstrapper.cs
@@ -1,16 +1,20 @@
 ﻿using Caliburn.Micro;
+using DarknetDiaries.Core;
+using DarknetDiaries.Standard;
 using DarknetDiaries.WinUI.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 
 namespace DarknetDiaries.WinUI
 {
    internal class Bootstrapper : BootstrapperBase
    {
+      #region Private
+      private SimpleContainer _Container = new SimpleContainer();
+      #endregion
+
       #region Bootstrapper basics
       public Bootstrapper()
       {
@@ -20,6 +24,22 @@ namespace DarknetDiaries.WinUI
       {
          DisplayRootViewFor<ShellViewModel>();
       }
+      protected override void Configure()
+      {
+         _Container.Singleton<IWindowManager, WindowManager>()
+            .Singleton<IEventAggregator, EventAggregator>()
+            .Singleton<SimpleContainer>();
+
+         _Container.PerRequest<ShellViewModel>()
+            .PerRequest<IEpisodeFeed, EpisodeFeed>();
+      }
+      protected override IEnumerable<Assembly> SelectAssemblies() => new[] { Assembly.GetExecutingAssembly() };
+      #endregion
+
+      #region Overrides for container
+      protected override object GetInstance(Type service, string key) => _Container.GetInstance(service, key);
+      protected override IEnumerable<object> GetAllInstances(Type service) => _Container.GetAllInstances(service);
+      protected override void BuildUp(object instance) => _Container.BuildUp(instance);
       #endregion
    }
 }
--- src/WinUI/ViewModels/ShellViewModel.cs
+++ src/WinUI/ViewModels/ShellViewModel.cs
@@ -1,13 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using DarknetDiaries.Standard;
 
 namespace DarknetDiaries.WinUI.ViewModels
 {
    internal class ShellViewModel : PropertyChangedBase
    {
+      #region Private
+      private readonly IEpisodeFeed _Feed;
+      private ISeriesInfo? _Info;
+      private ObservableCollection<IEpisode> _Episodes = new ObservableCollection<IEpisode>();
+      #endregion
+
+      #region Properties
+      public ISeriesInfo? Info { get => _Info; private set => Set(ref _Info, value); }
+      public ObservableCollection<IEpisode> Episodes => _Episodes;
+      #endregion
+      public ShellViewModel(IEpisodeFeed feed)
+      {
+         _Feed = feed;
+         RefreshFeed();
+      }
+
+      #region Methods
+      private void RefreshFeed()
+      {
+         Task task = _Feed.RefreshData();
+
+         task.ContinueWith(FeedRefreshed);
+      }
+      private void FeedRefreshed(Task task)
+      {
+         Info = _Feed.GetInfo();
+         var episodes = _Feed.GetEpisodes().ToList();
+
+         // replace or add episodes
+         for (int i = 0; i < episodes.Count; i++)
+         {
+            if (i < _Episodes.Count)
+               _Episodes[i] = episodes[i];
+            else
+               _Episodes.Add(episodes[i]);
+         }
+
+         // remove later ones | should never actually be necessary but used as a precaution
+         for (int i = _Episodes.Count - 1; i >= episodes.Count; i--)
+            _Episodes.RemoveAt(i);
+      }
+      #endregion
    }
 }
