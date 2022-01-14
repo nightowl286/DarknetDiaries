2846cd91042a9fa7926571f7ec3e117beb4910be
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2022-01-14 10:10:23 +0000

    Designed part of the UI.

--- src/WinUI/Views/ShellView.xaml.cs
+++ src/WinUI/Views/ShellView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DarknetDiaries.WinUI.Views
@@ -18,11 +18,22 @@ namespace DarknetDiaries.WinUI.Views
    /// <summary>
    /// Interaction logic for ShellView.xaml
    /// </summary>
-   public partial class ShellView : UserControl
+   public partial class ShellView : Window
    {
       public ShellView()
       {
          InitializeComponent();
       }
+
+      #region Events
+      private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+      {
+         OpenUrl(e.Uri.AbsoluteUri);
+      }
+      #endregion
+
+      #region Methods
+      private void OpenUrl(string url) => Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden });
+      #endregion
    }
 }
