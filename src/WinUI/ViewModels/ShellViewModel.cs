cce203cb732e42d4e512e0c28bc1ef5c42360467
Nightowl286 <79215414+nightowl286@users.noreply.github.com>
2022-01-14 11:55:48 +0000

    Made the shell track the next episode.

--- src/WinUI/ViewModels/ShellViewModel.cs
+++ src/WinUI/ViewModels/ShellViewModel.cs
@@ -17,11 +17,13 @@ namespace DarknetDiaries.WinUI.ViewModels
       private ISeriesInfo? _Info;
       private ObservableCollection<EpisodeViewModel> _Episodes = new ObservableCollection<EpisodeViewModel>();
       private bool _IsRefreshing;
+      private EpisodeViewModel? _NextEpisode;
       #endregion
 
       #region Properties
       public ISeriesInfo? Info { get => _Info; private set => Set(ref _Info, value); }
       public ObservableCollection<EpisodeViewModel> Episodes => _Episodes;
+      public EpisodeViewModel? NextEpisode { get => _NextEpisode; private set => Set(ref _NextEpisode, value); }
       #endregion
       public ShellViewModel(IEpisodeFeed feed, ITimeStorage timeStorage)
       {
@@ -63,6 +65,17 @@ namespace DarknetDiaries.WinUI.ViewModels
          // remove later ones | should never actually be necessary but used as a precaution
          for (int i = _Episodes.Count - 1; i >= episodes.Count; i--)
             _Episodes.RemoveAt(i);
+
+         // Select next episode
+         EpisodeViewModel? nextEp = null;
+         for(int i = _Episodes.Count - 1; i >= 0; i--)
+         {
+            if (nextEp?.IsFinished != true)
+            {
+               nextEp = _Episodes[i];
+               break;
+            }
+         }
 
          _IsRefreshing = false;
          NotifyOfPropertyChange(() => CanSynchronise);
